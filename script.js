2f3746d4be4c438f3fe036375c64a1a73d0fef24
Michael Johnson <mjj.yoyogi@gmail.com>
2022-07-13 12:26:18 -0400

    added password confirmation and some styling, need to fix up styling and general form then will be done

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-var check = function(){
 
-    
+
+function check(){
+    let password = document.getElementById('userPassword');
+
+    let confirmPassword = document.getElementById('confirmPassword');
+
+    if (password.value != confirmPassword.value){
+        confirmPassword.setCustomValidity("Passwords don't match");
+        confirmPassword.reportValidity();
+
+    }else{
+        confirmPassword.setCustomValidity('');
+        confirmPassword.reportValidity();
+
+    }
+
+
 }
+
+
